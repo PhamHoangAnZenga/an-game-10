42fce6ce03a18d12e422e09588b26e7f93929ea9
KNSBA <phamhoangan@zenga.com.vn>
2026-08-25 16:11:01 +0700

    add tutorial text

--- Joystick/Assets/Project/Prefabs/Gameplay/CollectableStar.cs
+++ Joystick/Assets/Project/Prefabs/Gameplay/CollectableStar.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CollectableStar : MonoBehaviour
 {
+    [SerializeField] UnityEvent _onActive;
+
     void OnTriggerEnter(Collider other)
     {
+        _onActive?.Invoke();        
         gameObject.SetActive(false);
     }
 }
